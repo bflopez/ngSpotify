3ac56ece8cce98918b95fc3708719c3fb8a72a48
Bryan Lopez <bryanlopez@Bryans-MacBook-Pro.local>
2017-02-24 15:23:40 -0500

    Initial Commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import {RouterModule, Routes} from '@angular/router'
+import {SearchComponent} from './components/search/search.component'
+import {AboutComponent} from './components/about/about.component'
+
+
+const routes: Routes = [
+  { path: '', component: SearchComponent},
+  { path: 'about', component: AboutComponent}
+];
